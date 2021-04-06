b3388c21b42354a4b02994968562f681b45b5555
Patrick Cuba <cubap@slu.edu>
2021-04-06 13:08:21 -0500

    Update manuscripts.mjs

--- js/manuscripts.mjs
+++ js/manuscripts.mjs
@@ -3,43 +3,70 @@ const DEFAULT_THUMB = "data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAA
 
 class GlossManuscript {
   constructor() {
-    this.is = performance.now().toString(32).replace(".","")
+    this.is = performance.now().toString(32).replace(".", "")
     this.id = undefined
     this.label = undefined
     this.thumb = DEFAULT_THUMB
     this.description = undefined
   }
-  setProperty(prop,value){
-    if(typeof prop !== "string") throw Error("Invalid Property. Expected `prop` to be a String.")
-    if(this[prop] === value) return
-    this[prop] = value
-    const updateEvent = new CustomEvent("manuscript-update",{
+  setProperty(prop, value) {
+    if (typeof prop !== "string") throw Error("Invalid Property. Expected `prop` to be a String.")
+    if (this[prop] === value) return
+    const updateEvent = new CustomEvent("manuscript-update", {
       detail: {
         is: this.is,
-        propertyName: prop
-
+        propertyName: prop,
+        oldVal: this[prop],
+        newVal: value
+      }
     })
+    document.dispatchEvent(updateEvent)
+    this[prop] = value
   }
+
+
 }
 
 class GlossManuscriptThumb extends HTMLElement {
   constructor() {
     super()
-    this.template = `
-    <header class="label">${this.label ?? ""}</header>
-    <img class="thumb" src="${this.thumb}">
-    <span class="description">${this.description}</span>
-    `
     this.ms = new GlossManuscript()
+    this.ms.id = this.getAttribute("glossing-id")
+    this.setAttribute("glossing-is",this.ms.is)
+    this.template = `<li glossing-is="${this.ms.is}" glossing-id="${this.ms.id}" class="loading">
+    <header class="label">${this.ms.label ?? ""}</header>
+    <img class="thumb" src="${this.ms.thumb}">
+    <span class="description">${this.ms.description}</span>
+    </li>
+    `
+  }
+
+  connectedCallback(){
+    this.render()
+  }
+
+  render() {
+    let item = document.querySelector(`[glossing-is="${this.ms.is}"]`)
+    if (!(item ?? false)) {throw Error("Attempted to render missing element.")}
+    const label = this.ms.name ?? this.ms.label ?? this.ms.title ?? false
+    if (label) {
+      item.querySelector('.label').innerHTML = label
+      item.classList.remove("loading")
+    }
+    _fetchLabel(this.ms["@id"] ?? this.ms.id).then(name => {
+      this.ms.name = name
+      this.render()
+    })
   }
 }
 
+customElements.define("gloss-manuscript-thumb", GlossManuscriptThumb)
 
 class GlossManuscripts extends HTMLElement {
   constructor() {
     super()
     const tmpl = `<ul class="tiled">
-        </ul>`
+    </ul>`
     this.innerHTML = tmpl
     this.collection = this.getAttribute("gloss-collection")
   }
@@ -84,154 +111,124 @@ class GlossManuscripts extends HTMLElement {
       })
       .then(this.render.bind(this))
   }
-  render(listOrItem) {
+  render(items) {
     const ul = this.querySelector(`ul`)
-    if (!(ul ?? false)) {
-      throw Error("List is missing!")
-    }
-    const renderItem = async (item) => {
-      let li = document.querySelector(`[glossing-id="${item["@id"] ?? item.id}"]`)
-      if (!(li ?? false)) {
-        let newItem = document.createElement(`li`)
-        newItem.setAttribute("glossing-id", item["@id"] ?? item.id)
-        newItem.classList.add("loading")
-        newItem.innerHTML = `
-        <header class="label"></header>
-        <img class="thumb">
-        <span class="description"></span>
-        `
-        ul.appendChild(newItem)
-        li = newItem
-      }
-      const label = item.name ?? item.label ?? item.title ?? false
-      if (label) {
-        li.querySelector('.label').innerHTML = label
-        li.classList.remove("loading")
-      }
-      this._fetchLabel(item["@id"] ?? item.id).then(name => {
-        item.name = name
-        renderItem(item)
-      })
-    }
-    if (!Array.isArray(listOrItem)) {
-      ul.innerHTML = ``
-      renderItem(listOrItem)
-      return
-    }
-    listOrItem.forEach(renderItem)
-  }
-
-  async _fetchImage(target) {
-
-  }
-
-  async _fetchLabel(target) {
-    const filterObj = { "__rerum.history.next": { $exists: true, $size: 0 } }
-    const queryObj = {
-      $and: [
-        {
-          $or: [
-            { target: target, },
-            { hasTarget: target, },
-            { 'target.value': target, }
-          ]
-        },
-        {
-          $or: [
-            { 'body.label': { $exists: true } },
-            { 'body.name': { $exists: true } },
-            { 'body.title': { $exists: true } }
-          ]
-        }
-      ]
-    }
-    Object.assign(queryObj, filterObj)
-    try {
-      const response = await fetch(RERUM_URL + "query", {
-        method: "POST",
-        mode: "cors",
-        body: JSON.stringify(queryObj),
-      })
-      const annos = await response.json()
-      for (const a of annos) {
-        const label = this._getLabel(a.body, "")
-        if (label?.length) {
-          return label
-        }
-      }
-      return "[ unlabeled ]"
-    } catch (err) {
-      return "[ label error ]"
-    }
-  }
-
-  _getLabel(obj, noLabel = "[ unlabeled ]", options = {}) {
-    if (typeof obj === "string") { return obj }
-    let label = obj[options.label] || obj.name || obj.label || obj.title
-    if (Array.isArray(label)) {
-      label = [...new Set(label.map(l => this._getValue(l)))]
-    }
-    if (typeof label === "object") {
-      label = this._getValue(label)
-    }
-    return label || noLabel
-  }
-
-  _getValue(property, asType, alsoPeek = []) {
-    // TODO: There must be a best way to do this...
-    let prop;
-    if (property === undefined || property === "") {
-      console.error("Value of property to lookup is missing!")
-      return undefined
-    }
-    if (Array.isArray(property)) {
-      // It is an array of things, we can only presume that we want the array.  If it needs to become a string, local functions take on that responsibility.
-      return property
-    } else {
-      if (typeof property === "object") {
-        // TODO: JSON-LD insists on "@value", but this is simplified in a lot
-        // of contexts. Reading that is ideal in the future.
-        if (!Array.isArray(alsoPeek)) {
-          alsoPeek = [alsoPeek]
-        }
-        alsoPeek = alsoPeek.concat(["@value", "value", "$value", "val"])
-        for (let k of alsoPeek) {
-          if (property.hasOwnProperty(k)) {
-            prop = property[k]
-            break
-          } else {
-            prop = property
-          }
-        }
-      } else {
-        prop = property
-      }
-    }
-    try {
-      switch (asType.toUpperCase()) {
-        case "STRING":
-          prop = prop.toString();
-          break
-        case "NUMBER":
-          prop = parseFloat(prop);
-          break
-        case "INTEGER":
-          prop = parseInt(prop);
-          break
-        case "BOOLEAN":
-          prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
-          break
-        default:
-      }
-    } catch (err) {
-      if (asType) {
-        throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
-      } else {
-        // no casting requested
-      }
-    }
-    return (prop.length === 1) ? prop[0] : prop
+    if (!(ul ?? false)) { throw Error("List is missing!") }
+    if (!Array.isArray(items)) { items = [items] }
+    ul.innerHTML = items.reduce((a, b) => a += `<gloss-manuscript-thumb glossing-id="${b['@id'] ?? b.id}"></gloss-manuscript-thumb>`, ``)
   }
 }
 
 customElements.define("gloss-manuscripts", GlossManuscripts)
+
+async function _fetchImage(target) {
+
+}
+
+async function _fetchLabel(target) {
+  const filterObj = { "__rerum.history.next": { $exists: true, $size: 0 } }
+  const queryObj = {
+    $and: [
+      {
+        $or: [
+          { target: target, },
+          { hasTarget: target, },
+          { 'target.value': target, }
+        ]
+      },
+      {
+        $or: [
+          { 'body.label': { $exists: true } },
+          { 'body.name': { $exists: true } },
+          { 'body.title': { $exists: true } }
+        ]
+      }
+    ]
+  }
+  Object.assign(queryObj, filterObj)
+  try {
+    const response = await fetch(RERUM_URL + "query", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(queryObj),
+    })
+    const annos = await response.json()
+    for (const a of annos) {
+      const label = this._getLabel(a.body, "")
+      if (label?.length) {
+        return label
+      }
+    }
+    return "[ unlabeled ]"
+  } catch (err) {
+    return "[ label error ]"
+  }
+}
+
+function _getLabel(obj, noLabel = "[ unlabeled ]", options = {}) {
+  if (typeof obj === "string") { return obj }
+  let label = obj[options.label] || obj.name || obj.label || obj.title
+  if (Array.isArray(label)) {
+    label = [...new Set(label.map(l => this._getValue(l)))]
+  }
+  if (typeof label === "object") {
+    label = this._getValue(label)
+  }
+  return label || noLabel
+}
+
+function _getValue(property, asType, alsoPeek = []) {
+  // TODO: There must be a best way to do this...
+  let prop;
+  if (property === undefined || property === "") {
+    console.error("Value of property to lookup is missing!")
+    return undefined
+  }
+  if (Array.isArray(property)) {
+    // It is an array of things, we can only presume that we want the array.  If it needs to become a string, local functions take on that responsibility.
+    return property
+  } else {
+    if (typeof property === "object") {
+      // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+      // of contexts. Reading that is ideal in the future.
+      if (!Array.isArray(alsoPeek)) {
+        alsoPeek = [alsoPeek]
+      }
+      alsoPeek = alsoPeek.concat(["@value", "value", "$value", "val"])
+      for (let k of alsoPeek) {
+        if (property.hasOwnProperty(k)) {
+          prop = property[k]
+          break
+        } else {
+          prop = property
+        }
+      }
+    } else {
+      prop = property
+    }
+  }
+  try {
+    switch (asType.toUpperCase()) {
+      case "STRING":
+        prop = prop.toString();
+        break
+      case "NUMBER":
+        prop = parseFloat(prop);
+        break
+      case "INTEGER":
+        prop = parseInt(prop);
+        break
+      case "BOOLEAN":
+        prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+        break
+      default:
+    }
+  } catch (err) {
+    if (asType) {
+      throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+    } else {
+      // no casting requested
+    }
+  }
+  return (prop.length === 1) ? prop[0] : prop
+}
